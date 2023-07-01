807338d0cbc767e34441fb0828664caa96cdc6eb
Maires Souza <trabalho.maires@gmail.com>
2023-07-01 13:01:48 -0300

    feat: implement function vqv

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,17 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
+const vqv = (name, age) => {
+  if (name === undefined && age === undefined) {
+    return undefined;
+  }
+
+  const vqvMessage = `Oi, meu nome é ${name}!
+Tenho ${age} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+
+return vqvMessage;
+};
 
 module.exports = vqv;
