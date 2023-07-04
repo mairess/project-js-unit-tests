10d190cc227cf28e4dfc038c1ca242132e0a4499
Maires Souza <trabalho.maires@gmail.com>
2023-07-03 21:16:41 -0300

    feat: tests and functions implemented for restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,32 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
+const createMenu = (object) => { 
+    const consumptionArray = [];
+    const fetchMenu = () => object;
+    const order = (string) => {
+        for (const key of Object.keys(object)) {
+            if (string in object[key]) {
+                consumptionArray.push(string);
+                return consumptionArray;
+            }
+        }
+        throw new Error('Item indisponível');
+    };
+    const pay = () => {
+        let total = 0;
+        const itemsObj = Object.values(object).reduce((acumulator, item) => ({ ...acumulator, ...item }), {});
 
-module.exports = createMenu;
+        for (const item of consumptionArray) {
+                if (item in itemsObj) {
+                    total += itemsObj[item];
+                }
+        }
+        const totalFixed = (total + (total * 0.1)).toFixed(2);
+        return Number(totalFixed);
+    };
+    const obj = { fetchMenu, consumption: consumptionArray, order, pay };
+    return obj;
+};
+
+module.exports = { createMenu };
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -1,8 +1,31 @@
-const createMenu = require('../src/restaurant');
+const { createMenu } = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // Escreva todos os testes aqui.
+    expect(createMenu()).toHaveProperty('fetchMenu');
+    expect(typeof(createMenu().fetchMenu)).toBe('function');
+    expect(createMenu({ food: {}, drinks: {} }).fetchMenu()).toStrictEqual({ food: {}, drinks: {} });
+    expect(createMenu({ food: {}, drinks: {} }).fetchMenu()).toMatchObject({ food: {}, drinks: {} });
+    expect(createMenu({ food: {}, drinks: {} }).consumption).toEqual([]);
+    const objetoRetornadoCreateMenu = createMenu({ food: { coxinha: 3.90, sanduiche: 9.90 }, drinks: { agua: 3.90, cerveja: 6.90 } });
+    expect(() => objetoRetornadoCreateMenu.order('café')).toThrow(Error(('Item indisponível')));
+    try {
+      objetoRetornadoCreateMenu.order('leite');
+    } catch (error) {
+      expect(objetoRetornadoCreateMenu.consumption).toHaveLength(0);
+      expect(objetoRetornadoCreateMenu.consumption).toStrictEqual([]);
+    }
+    objetoRetornadoCreateMenu.order('coxinha');
+    objetoRetornadoCreateMenu.order('sanduiche');
+    objetoRetornadoCreateMenu.order('agua');
+    expect(objetoRetornadoCreateMenu.consumption).toHaveLength(3);
+    expect(objetoRetornadoCreateMenu.consumption).toStrictEqual([ 'coxinha', 'sanduiche', 'agua' ]);
+    objetoRetornadoCreateMenu.order('coxinha');
+    expect(objetoRetornadoCreateMenu.consumption).toHaveLength(4);
+    expect(objetoRetornadoCreateMenu.consumption).toStrictEqual([ 'coxinha', 'sanduiche', 'agua', 'coxinha' ]);
+    expect(objetoRetornadoCreateMenu.pay()).toEqual(23.76);
+    objetoRetornadoCreateMenu.order('cerveja');
+    expect(objetoRetornadoCreateMenu.pay()).toEqual(31.35);
   });
 });
